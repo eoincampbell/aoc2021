bfa5c866fed5e10239d3015dc424ea1b72f711ec
Eoin Campbell <eoin.campbell@channelsight.com>
2021-12-11 14:30:59 +0000

    Day 11 Cleanup

--- Aoc2021/Puzzles/Day11.cs
+++ Aoc2021/Puzzles/Day11.cs
@@ -1,113 +1,90 @@
-﻿using Aoc2021.Puzzles.Day11Extensions;
+﻿using System.Linq;
+using Aoc2021.Puzzles.Day11Extensions;
 
 namespace Aoc2021.Puzzles
 {
     internal class Day11 : Puzzle
     {
+        //Toggle to Show State after each Step;
+        public static bool EnablePrinting => false;
+
         public Day11()
-        : base("Inputs/Day11.txt")
-        //: base("Inputs/Day11Sample.txt")
-        //: base("Inputs/Day11Sample2.txt")
+            : base("Inputs/Day11.txt")
         {
-            _map = PuzzleInput
-                .Select(y => y.Select(x => int.Parse(x.ToString())))
-                .To2DArray();
+            _map = PuzzleInput.Select(y => y.Select(x => int.Parse(x.ToString()))).To2DArray();
         }
 
         private static (int y, int x)[] CoordModifiers => new[] { (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1) };
-        private static bool EnablePrinting => false;
         private readonly int[,] _map;
         private int MapWidth => _map.GetUpperBound(0);
         private int MapHeight => _map.GetUpperBound(1);
         private int _flashCount;
-        //Toggle to Show State after each Step;
 
         private object Part1()
         {
-            _flashCount = 0;
-            _map.Dump(EnablePrinting, 0);
-
-            for (int i = 1; i <= 100; i++) //steps 1-100
-                Step(i);
+            _map.Dump(0);
+            for (var step = 1; step <= 100; step++) //steps 1-100
+                Step(step);
 
             return _flashCount;
         }
 
         private object Part2()
         {
-            for (int i = 101; i <= 400; i++) //steps 101-400
+            var step = 100;
+            do
             {
-                Step(i);
-                if (BigFlash()) return i;
-            }
-
-            return -1;
+                Step(step++);
+            } while (!BigFlash());
+            return step;
         }
 
         private void Step(int i)
         {
             //Increase everyone by 1
-            for (int y = 0; y <= MapHeight; y++ )
-                for(int x = 0; x <= MapWidth; x++)
+            for (int y = 0; y <= MapHeight; y++)
+                for (int x = 0; x <= MapWidth; x++)
                     _map[y, x]++;
 
             //Find any >9's that havent yet been processed
             //they might get processed internally in the recursive loops so this is just a starting point and covers stragglers
             for (int y = 0; y <= MapHeight; y++)
                 for (int x = 0; x <= MapWidth; x++)
-                    if(_map[y,x] > 9)
+                    if(_map[y, x] > 9)
                         Flash(y, x);
 
-            //Find any -1's that have flashed and reset to 0;
-            for (int y = 0; y <= MapHeight; y++)
-                for (int x = 0; x <= MapWidth; x++)
-                    if (_map[y, x] == -1)
-                        _map[y, x] = 0;
-
-            _map.Dump(EnablePrinting, i);
+            _map.Dump(i);
         }
 
         private void Flash(int y, int x)
         {
             //this guy doesn't flash, move on
-            if (_map[y, x] <= 9)
-                return;
+            if (_map[y, x] <= 9) return;
 
             //Flash the current octopus and record it.
-            _map[y, x] = -1;
+            _map[y, x] = 0;
             _flashCount++;
 
             foreach (var m in CoordModifiers)
             {
-                //check that the modifer is in bounds or bail.
-                if (!IsInBounds(y, x, m.y, m.x))
-                    continue;
+                if (!IsInBounds(y, x, m.y, m.x)) continue; //check that the modifer is in bounds or bail.
 
-                int ny = y + m.y;
-                int nx = x + m.x;
+                int ny = y + m.y, nx = x + m.x;
 
-                //for anyone who hasn't flashed this step, bump their energy
-                if (_map[ny, nx] != -1)
-                    _map[ny, nx]++;
+                if (_map[ny, nx] != 0) _map[ny, nx]++; //increase energy of non-flashed
 
-                //for any who are now over energy 9, recursively process them
-                if (_map[ny, nx] > 9)
-                    Flash(ny, nx);
+                if (_map[ny, nx] > 9) Flash(ny, nx); //flash if ready to
             }
         }
 
         private bool IsInBounds(int y, int x, int my, int mx) =>
-            y + my >= 0 && y + my <= MapHeight && x + mx >= 0 && x + mx <= MapWidth;
+            y + my >= 0 &&
+            y + my <= MapHeight &&
+            x + mx >= 0 &&
+            x + mx <= MapWidth;
 
-        private bool BigFlash()
-        {
-            int sum = 0;
-            for (int y = 0; y <= MapHeight; y++)
-                for (int x = 0; x <= MapWidth; x++)
-                    sum += _map[y, x];
-
-            return sum == 0;
-        }
+        private bool BigFlash() => _map.Cast<int>().Sum() == 0;
+        
 
         protected override string Title => "################ Day 11 ####################";
         protected override object RunPart1() => Part1(); //1669  (Sample: 1656)
@@ -118,9 +95,9 @@ namespace Aoc2021.Puzzles
     {
         internal static class Extensions
         {
-            internal static void Dump(this int[,] map, bool EnablePrinting, int step)
+            internal static void Dump(this int[,] map, int step)
             {
-                if (!EnablePrinting) return;
+                if (!Day11.EnablePrinting) return;
 
                 Console.WriteLine("-------------------------------------------");
                 Console.WriteLine($"After Step: {step}");
