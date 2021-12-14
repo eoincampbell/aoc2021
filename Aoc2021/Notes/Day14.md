63323bba0a7582e3927622a3f2f39a50be161b21
Eoin Campbell <eoin.campbell@channelsight.com>
2021-12-14 12:40:56 +0000

    Day 14 Cleanup

---
+++ Aoc2021/Notes/Day14.md
@@ -0,0 +1,31 @@
+﻿
+
+    //Old Part 1
+
+    //private object Part1()
+    //{
+    //    LoadInsertionRules();
+    //    var polymer = PuzzleInput.FirstOrDefault()!;
+
+    //    for (int step = 1; step <= 10; step++)
+    //    {
+    //        var newElements = new List<string>();
+    //        for (int i = 0; i < polymer.Length - 1; i++)
+    //            newElements.Add(_insertionRules[polymer[i..(i + 2)]]);
+
+    //        var output = polymer
+    //            .Select(x => x.ToString())
+    //            .ZipWithDefault(newElements, (f, s) => new[] { f, s })
+    //            .SelectMany(f => f);
+
+    //        polymer = string.Concat(output);
+    //    }
+
+    //    var elements = polymer
+    //        .GroupBy(g => g)
+    //        .Select(y => new { y.Key, Count = y.Count() })
+    //        .OrderByDescending(o => o.Count)
+    //        .ToList();
+
+    //    return elements[0].Count - elements[^1].Count;
+    //}
--- Aoc2021/Puzzles/Day14.cs
+++ Aoc2021/Puzzles/Day14.cs
@@ -9,9 +9,7 @@
         private readonly Dictionary<string, string> _insertionRules;
         private string _polymer => PuzzleInput.FirstOrDefault()!;
 
-        public Day14()
-            : base("Inputs/Day14.txt")
-            //: base("Inputs/Day14Sample.txt")
+        public Day14(): base("Inputs/Day14.txt")
         {
             _insertionRules = new();
             foreach (var line in PuzzleInput.Skip(2))
@@ -51,34 +49,4 @@
             return el.Max(m => m.Value) - el.Min(m => m.Value);         //grab the diff between the largest and smallest
         }
     }
-
-    //Old Part 1
-
-    //private object Part1()
-    //{
-    //    LoadInsertionRules();
-    //    var polymer = PuzzleInput.FirstOrDefault()!;
-
-    //    for (int step = 1; step <= 10; step++)
-    //    {
-    //        var newElements = new List<string>();
-    //        for (int i = 0; i < polymer.Length - 1; i++)
-    //            newElements.Add(_insertionRules[polymer[i..(i + 2)]]);
-
-    //        var output = polymer
-    //            .Select(x => x.ToString())
-    //            .ZipWithDefault(newElements, (f, s) => new[] { f, s })
-    //            .SelectMany(f => f);
-
-    //        polymer = string.Concat(output);
-    //    }
-
-    //    var elements = polymer
-    //        .GroupBy(g => g)
-    //        .Select(y => new { y.Key, Count = y.Count() })
-    //        .OrderByDescending(o => o.Count)
-    //        .ToList();
-
-    //    return elements[0].Count - elements[^1].Count;
-    //}
 }
