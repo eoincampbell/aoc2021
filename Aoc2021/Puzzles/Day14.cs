017a3cdfb79a366af52c79cdba80d95545deee1d
Eoin Campbell <eoin.campbell@channelsight.com>
2021-12-14 12:38:47 +0000

    Day 14 Cleanup

--- Aoc2021/Puzzles/Day14.cs
+++ Aoc2021/Puzzles/Day14.cs
@@ -3,21 +3,17 @@
     internal class Day14 : Puzzle
     {
         public override int Day => 14;
-        protected override object RunPart1() => Part1(); //3143
-        protected override object RunPart2() => Part2(); //4110215602456
+        protected override object RunPart1() => Process(10); //3143
+        protected override object RunPart2() => Process(40); //4110215602456
 
-        private Dictionary<string, string> _insertionRules = new();
+        private readonly Dictionary<string, string> _insertionRules;
+        private string _polymer => PuzzleInput.FirstOrDefault()!;
 
         public Day14()
             : base("Inputs/Day14.txt")
-        //: base("Inputs/Day14Sample.txt")
+            //: base("Inputs/Day14Sample.txt")
         {
-        }
-
-        private void LoadInsertionRules()
-        {
-            _insertionRules = new Dictionary<string, string>();
-
+            _insertionRules = new();
             foreach (var line in PuzzleInput.Skip(2))
             {
                 var (source, target, _) = line.Split(" -> ");
@@ -25,80 +21,64 @@
             }
         }
 
-        private object Part1()
+        private object Process(int steps)
         {
-            LoadInsertionRules();
-            var polymer = PuzzleInput.FirstOrDefault()!;
-
-            for (int step = 1; step <= 10; step++)
-            {
-                var newElements = new List<string>();
-                for(int i = 0; i < polymer.Length -1; i++)
-                    newElements.Add(_insertionRules[polymer[i..(i + 2)]]);
-
-                var output = polymer
-                    .Select(x => x.ToString())
-                    .ZipWithDefault(newElements, (f, s) => new[] { f, s })
-                    .SelectMany(f => f);
-
-                polymer = string.Concat(output);
-            }
-
-            var elements = polymer
-                .GroupBy(g => g)
-                .Select(y => new { y.Key, Count = y.Count() })
-                .OrderByDescending(o => o.Count)
-                .ToList();
-
-            return elements[0].Count - elements[^1].Count;
-        }
-
-        private object Part2()
-        {
-            LoadInsertionRules();
-            var polymer = PuzzleInput.FirstOrDefault()!;
-
-            //create a dict to track counts of all pairs
             var pairs = Enumerable
-                .Range(0, polymer.Length - 1)
-                .Select(idx => polymer[idx..(idx + 2)]) //Yay IndexRanges
-                .GroupBy(p => p)
-                .ToDictionary(d => d.Key, d => d.LongCount());
+                .Range(0, _polymer.Length - 1)                          // 10 char polymer has 9 pairs
+                .Select(idx => _polymer[idx..(idx + 2)])                // foreach generate a consecutive pair... Yay IndexRanges
+                .GroupBy(p => p)                                        // Group the pairs (incase their dupes)
+                .ToDictionary(d => d.Key, d => d.LongCount());          // Add the Pairs to a tracking dict with their occurrence count
 
-            //track char counts in a different dict.
-            var charCounts = polymer
-                .GroupBy(p => p)
+            var el = _polymer                                           // Also add each char element to a tracking dicct
+                .GroupBy(p => p)                                        // for tracking the occurrence count
                 .ToDictionary(d => d.Key.ToString(), d => d.LongCount());
 
-            for (int step = 1; step <= 40; step++)
+            for (int step = 1; step <= steps; step++)                   //Step
             {
-                //each step, we're going to end up with a new set of pairs.
-                //go through all the pairs we currently have
-                    //look it up in the dictionary of polymer formulae
-                    //split it and make 2 new pairs.
-                    //e.g.NX -> NC and CX by injecting 'C'
-                    //find NX in our pair counter and get the current count
-                    //find NC in our pair counter (or add) and increment by old NX value
-                    //find CX in our pair counter(or add) and increment by old NX value
-                    //find C in our char counter (or) add and increment by old NX value
-
-                var newPairs = new Dictionary<string, long>();
-
-                foreach (var oldPair in pairs)
+                var np = new Dictionary<string, long>();                //Dict to hold new pairs cos modifying in foreach is bad
+                foreach (var (key, cnt) in pairs)                       //foreach current pair
                 {
-                    var ele = _insertionRules[oldPair.Key];
-                    string f = oldPair.Key[0] + ele
-                        , s = ele + oldPair.Key[1];
+                    var e = _insertionRules[key];                       //find out what goes between it
+                    string f = key[0] + e,                              //create the 2 new pairs with that new element
+                           s = e + key[1];
 
-                    var c = oldPair.Value;
-                    newPairs[f] = newPairs.ContainsKey(f) ? (newPairs[f] + c) : c;
-                    newPairs[s] = newPairs.ContainsKey(s) ? (newPairs[s] + c) : c;
-                    charCounts[ele] = charCounts.ContainsKey(ele) ? (charCounts[ele] + c) : c;
+                    np[f] = np.ContainsKey(f) ? (np[f] + cnt) : cnt;    //add or inc. the count for those new pairs
+                    np[s] = np.ContainsKey(s) ? (np[s] + cnt) : cnt;
+                    el[e] = el.ContainsKey(e) ? (el[e] + cnt) : cnt;    //also increase the count for the number of occurences of that element
                 }
-
-                pairs = newPairs;
+                pairs = np;                                             //load our new pairs for the next iteration
             }
-            return charCounts.Max(m => m.Value) - charCounts.Min(m => m.Value);
+            return el.Max(m => m.Value) - el.Min(m => m.Value);         //grab the diff between the largest and smallest
         }
     }
+
+    //Old Part 1
+
+    //private object Part1()
+    //{
+    //    LoadInsertionRules();
+    //    var polymer = PuzzleInput.FirstOrDefault()!;
+
+    //    for (int step = 1; step <= 10; step++)
+    //    {
+    //        var newElements = new List<string>();
+    //        for (int i = 0; i < polymer.Length - 1; i++)
+    //            newElements.Add(_insertionRules[polymer[i..(i + 2)]]);
+
+    //        var output = polymer
+    //            .Select(x => x.ToString())
+    //            .ZipWithDefault(newElements, (f, s) => new[] { f, s })
+    //            .SelectMany(f => f);
+
+    //        polymer = string.Concat(output);
+    //    }
+
+    //    var elements = polymer
+    //        .GroupBy(g => g)
+    //        .Select(y => new { y.Key, Count = y.Count() })
+    //        .OrderByDescending(o => o.Count)
+    //        .ToList();
+
+    //    return elements[0].Count - elements[^1].Count;
+    //}
 }
