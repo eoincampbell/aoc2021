6791b8b0760113c3e8cafdf415882d424caa977b
Eoin Campbell <eoin.campbell@channelsight.com>
2021-12-15 16:30:24 +0000

    Day 15 Cleanup

---
+++ Aoc2021/Puzzles/Day16.cs
@@ -0,0 +1,18 @@
+﻿using Aoc2021.Puzzles.Day15Extensions;
+namespace Aoc2021.Puzzles
+{
+    internal class Day16 : Puzzle
+    {
+        public override int Day => 16;
+        protected override object RunPart1() => DoStuff();   //
+        protected override object RunPart2() => DoStuff();   //
+
+        public Day16()
+            : base("Inputs/Day16.txt")
+            //: base("Inputs/Day16Sample.txt")
+        {
+        }
+
+        public static object DoStuff() => -1;
+    }
+}
